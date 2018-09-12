2b11f487f881af50628da0ec9ed8b92f8b59e1f4
lee-warrick <lee.warrick@capco.com>
2018-09-12 10:41:27 -0400

    add algorithm and fix resizing

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -19,8 +19,8 @@ export class ContainerComponent implements OnInit {
     this.imageArr = this.images.map((x: any, i: number) => {
       let y: any = {};
       i === 3 ? y.enlarged = true : y.enlarged = false;
-      y.color = "https://picsum.photos/512/288/?image=" + x.id;
-      y.gray = "https://picsum.photos/g/512/288/?image=" + x.id;
+      y.color = "https://picsum.photos/512/300/?image=" + x.id;
+      y.gray = "https://picsum.photos/g/512/300/?image=" + x.id;
       y.id = x.id;
       y.src = y.gray;
       return y;
@@ -28,17 +28,38 @@ export class ContainerComponent implements OnInit {
   }
 
   handleEmission(id) {
-    this.imageArr = this.imageArr.map((x:any) => {
-      console.log(x);
-      if(x.id === id) {
+    let focusItemIndex;
+    let prevEnlargedIndex = this.imageArr.reduce((accumulator: any, current: any, index: number) => {
+      if(current.enlarged) {
+        accumulator = index;
+      }
+      return accumulator;
+    })
+    let newImageArr = this.imageArr.map((x: any, i: number) => {
+      if (x.id === id) {
         x.src = x.color;
         x.enlarged = true;
+        focusItemIndex = i;
       } else {
         x.src = x.gray;
         x.enlarged = false;
       }
       return x;
     })
+
+    if (focusItemIndex > 4) {
+      let moveTo;
+      let shiftImg = newImageArr.splice(focusItemIndex, 1);
+      if (focusItemIndex - prevEnlargedIndex === 4) {
+        moveTo = focusItemIndex - 5;
+      } else {
+        moveTo = focusItemIndex - 4;
+      }
+      newImageArr.splice(moveTo, 0, shiftImg[0]);
+    }
+
+    this.imageArr = newImageArr;
   }
+
 
 }
